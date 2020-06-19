d8ebed2c041e80c9350090b6d892c19bba09bdf5
Snick <nickamorg0@gmail.com>
2020-06-19 16:34:31 +0300

    More Country flags

---
+++ src/CountryComponent.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Svg, {Pattern, Path, Defs, Image} from 'react-native-svg';
+import { countrySvgs } from './countrySvgs';
+
+class CountryComponent extends Component {
+
+    render = () => {
+        const isCompleted = this.props.isCompleted;
+        const continentID = this.props.continentID;
+        const countryID = this.props.countryID;
+
+        return (
+            <Svg width='70%' height='70%' viewBox={countrySvgs[continentID][countryID].viewBox}>
+                <Defs>
+                    <Pattern id='pattern' preserveAspectRatio='xMidYMid slice' width='100%' height='100%' viewBox='0 0 500 300'>
+                        <Image width='500' height='300' href={countrySvgs[continentID][countryID].picURI}/>
+                    </Pattern>
+                </Defs>
+                <Path d={countrySvgs[continentID][countryID].d} transform={countrySvgs[continentID][countryID].transform} fill={isCompleted == 1 ? 'url(#pattern)' : '#f2f2f2'}/>
+            </Svg>
+        );
+    }
+}
+
+export default CountryComponent;
